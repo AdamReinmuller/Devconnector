2bf1383ca94c0f154e9fdbdb00f2ad8ab62d705e
Adam Rajmuller <rajfta@gmail.com>
2019-08-05 21:25:57 +0200

    login, register

--- server.py
+++ server.py
@@ -4,8 +4,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
